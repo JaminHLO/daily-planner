3bdc9d2b314c64bfa17f237875549748e903d8c4
Jaimon <jamin.hogan@gmail.com>
2023-02-23 00:45:00 -0500

    created mostly correct date and highlight color functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var currentDayElem = $('#currentDay');
+// var dailyPlannerElem = $('#daily-planner');
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +24,60 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+//display current state of planner
+function displayPlanner (plannerEntries) {
+  const workHoursArray = ["hour-9", "hour-10","hour-11","hour-12","hour-13","hour-14","hour-15","hour-16","hour-17"];
+  $.each(workHoursArray, function(index, hour) {
+    var curPlannerElem = $("#"+`${hour}`);
+    // var curHour =  dayjs().format('H');
+    var curHour = 12;
+    console.log("at", `${index}`, "the id is", "#"+`${hour}`);
+    console.log("the hour is", curHour);
+    if ((curHour-9) < `${index}`) {
+      console.log("hasn't happened yet");
+      curPlannerElem.removeClass();
+      curPlannerElem.addClass('row time-block past');
+    } else if ((curHour-9) == `${index}`) {
+      console.log("happening now");
+      curPlannerElem.removeClass();
+      curPlannerElem.addClass('row time-block present');
+    } else {
+      console.log ("happening in future");
+      curPlannerElem.removeClass();
+      curPlannerElem.addClass('row time-block future');
+    }
+    //if plannerEntries has content for this hour, load into textarea
+  });
+}
+
+//load planner entries
+function loadPlannerEntries () {
+  var dailyPlanner = localStorage.getItem('daily-planner');
+  if (dailyPlanner) {
+    dailyPlanner = JSON.parse(dailyPlanner);
+  }
+  else {
+    dailyPlanner = [];
+    console.log("nothing in localStorage for daily-planner");
+  }
+  return dailyPlanner;
+}
+
+function init () {
+  //do stuff
+  console.log("inside init");
+  //set current day
+  // const advancedFormat = require('dayjs/plugin/advancedFormat');
+  // dayjs.extend(advancedFormat);
+  let currentTime = dayjs().format('dddd, MMMM Do');
+  console.log(currentTime);
+  currentDayElem.text(currentTime);
+
+  //load entries and return daily planner
+  var plannerEntries = loadPlannerEntries ();
+  displayPlanner(plannerEntries);
+
+}
+
+init ();
